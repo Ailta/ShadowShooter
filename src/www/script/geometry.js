c39ca0e8f3cc280bb4fc02c9cef55f7df7c32d2f
Jakub_Polák <polakj21@spstrutnov.cz>
2024-01-13 10:25:48 +0100

    draw only what is seen by player

--- src/www/script/geometry.js
+++ src/www/script/geometry.js
@@ -2,16 +2,47 @@ function polygon(points) {
    return {
       points: points,
       draw: function(ctx, offX, offY) {
-         ctx.beginPath();
-         for (point of this.points) {
-            ctx.lineTo(offX + point[0], offY + point[1]);
+         let inWiew = false;
+
+         // check if players direction goes into any of its sides
+         for (let i=0; i< this.points.length; i++) {
+            let p1 = this.points[i];
+            let p2 = this.points[(i + 1) % this.points.length];
+
+            let ang1 = Math.atan2(p1[1], p1[0]);
+            let ang2 = Math.atan2(p2[1], p2[0]);
+
+            if (ang1 < player.direction && ang2 > player.direction || ang1 > player.direction && ang2 < player.direction) {
+               inWiew = true;
+               break;
+            }
          }
-         ctx.fillStyle = "crimson";
-         ctx.fill();
+
+         if (!inWiew) {
+            // check if any point in players field of view
+            for (point of this.points) {
+               let ang = Math.atan2(point[1], point[0]);
+
+               if (ang < player.direction + player.fov && ang > player.direction - player.fov) {
+                  inWiew = true;
+                  break;
+               }
+            }
+         }
+
+         if (inWiew) {
+            ctx.beginPath();
+            for (point of this.points) {
+               ctx.lineTo(offX + point[0], offY + point[1]);
+            }
+            ctx.fillStyle = "crimson";
+            ctx.fill();
+         }
       }
    }
 }
 
 let map = [
-   polygon([[100, 200], [200, 200], [200, 100], [100, 100]]),
+   polygon([[-300, 200], [200, 200], [200, 100], [-300, 100]]),
+   polygon([[-300, -200], [200, -200], [200, -100], [-300, -100]]),
 ]
